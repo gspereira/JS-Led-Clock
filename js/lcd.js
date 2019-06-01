d58a308b0d527b8cbe48cca4585763eab1241516
Gustavo Pereira <gustavopereira233@gmail.com>
2019-06-01 11:54:56 -0300

    Trying to change the clock logic

--- js/lcd.js
+++ js/lcd.js
@@ -1,6 +1,6 @@
 //Define o desenho de cada número manualmente, para aumentar o tamanho
 //os caracteres são duplicados.
-var digits={
+var digits = {
   0: ['-', '|', '|', ' ', '|', '|', '-'],
   1: [' ', ' ', '|', ' ', ' ', '|', ' '],
   2: ['-', ' ', '|', '-', '|', ' ', '-'],
@@ -14,7 +14,7 @@ var digits={
   '-': [' ', ' ', ' ', '-', ' ', ' ', ' '],
 };
 
-// HorizontalId é a posição que a função vai pegar de cada digito na array Digits
+// horizontalId é a posição que a função vai pegar de cada digito na array Digits
 // Funcao horizontal vai imprimir uma linha de _ por vez, recebe qual linha ele vai usa no horizontalId, o numero completo para converter no numero, e usa tamanho para multiplicar a quantidade de caracteres.
 function hor(horizontalId, numero, tamanho) {
   var texto="";
@@ -28,10 +28,10 @@ function hor(horizontalId, numero, tamanho) {
       texto+=digits[digit][horizontalId];      
     }
    // Adiciona o espaçamento depois caracteres 
-    texto += '  ';
-  }
-  texto += '\n';
-  return texto;
+   texto += '  ';
+ }
+ texto += '\n';
+ return texto;
 }
 // verticalId é a possição que a função vai pegar de cada digito na array Digits
 // Funcao vertical vai imprimir uma linha de | por vez, recebe qual linha ele vai usa no verticalId, o numero completo para converter no numero, e usa tamanho para multiplicar a quantidade de linebreaks.
@@ -75,26 +75,21 @@ function refresh(event) {
   var timezone = document.getElementById('fusoHorario').value;
   var tam = document.getElementById('tamanho').value;
   if (timezone == 'local') {
-    display.textContent = formatarTexto(getTime(), tam);
+    display.textContent = formatarTexto(getTimeLocal(), tam);
   } else {
     getTimeFuso(timezone, tam);
   }
-  var t = setTimeout(refresh, 1000);
+  //clock();
+  //var t = setTimeout(refresh, 1000);
 }
 
 // Retorna o horário local
-function getTime() {
+function getTimeLocal() {
   var hora = new Date();
   var h = hora.getHours();
   var m = hora.getMinutes();
   var s = hora.getSeconds();
-  return checkTime(h) + '-' +  checkTime(m) + '-' + checkTime(s);
-}
-
-// Adiciona o 0 na frente dos números menores que 10
-function checkTime(i) {
-  if (i < 10) {i = "0" + i};
-  return i;
+  return setHora(h,m,s);
 }
 
 //Faz uma requisição à API de qual o horário da região escolhida na view
@@ -110,15 +105,66 @@ function getTimeFuso(timezone, tam) {
     var h = hora.getHours();
     var m = hora.getMinutes();
     var s = hora.getSeconds();
-    var tempo = checkTime(h) + '-' +  checkTime(m) + '-' + checkTime(s);
+    var tempo = setHora(h,m,s);
     display.textContent = formatarTexto(tempo, tam);
   }
   request.send();
 }
+
+// Adiciona o 0 na frente dos números menores que 10
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};
+  return i;
+}
+
+// Aqui define a hora dos inputs escondidos
+function setHora(h, m, s) {
+  document.getElementById('h').value = h;
+  document.getElementById('m').value = m;
+  document.getElementById('s').value = s;
+
+  return checkTime(h) + '-' +  checkTime(m) + '-' + checkTime(s);
+}
+
+// Aqui pega a hora dos inputs escondidos no html
+function getHora() {
+  var h = document.getElementById('h').value;
+  var m = document.getElementById('m').value;
+  var s = document.getElementById('s').value;
+
+  return {
+    'h': checkTime(h),
+    'm': checkTime(m),
+    's': checkTime(s), 
+  };
+}
+
+// Estou tentando fazer essa parte do código auto-iterar o valor
+function clock() {
+  var hora = getHora();
+
+  hora['s']++;
+  if(hora['s'] == 60){
+    hora['s'] = 0;
+    hora['m']++;
+    if(hora['m'] == 60){
+      hora['m'] = 0;
+      hora['h']++;
+      if(hora['h'] == 24){
+        hora['h'] = 0;
+      }
+    }
+  }
+  var tam = document.getElementById('tamanho').value;
+  display = document.getElementById('display');
+  display.textContent = formatarTexto(setHora(hora['h'],hora['m'],hora['s']),tam);
+  var clock = setTimeout(clock, 1000);
+};
 
 // Verifica se há alguma alteração na view
 onload = function() {
   document.getElementById('fusoHorario').onchange = refresh;
   document.getElementById('tamanho').onchange = refresh;
   refresh();
+  clock();
 }
